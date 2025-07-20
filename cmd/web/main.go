e5b4a0aac2d764ffbca78f2acdfcbf5505621414
zakkbob <114868973+zakkbob@users.noreply.github.com>
2025-07-20 20:43:26 +0100

    Chapter 2.9 - Serving static files
    Serve static files under '/static/' using http.FileServer from directory ./ui/static. Add css, js, and images to home page. Disable directory listing in '/static/' using one simple trick! (not in subdirs though, im lazy)

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -9,6 +9,13 @@ func main() {
 	// Initialise a 'servemux', this is where route handlers will be registered
 	mux := http.NewServeMux()
 
+	// Create a file server, to serve files out of ui/static
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+
+	// Register fileServer as the handler for all paths starting with '/static/'
+	// Remove the 'static/' prefix before it reaches the fileserver
+	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
+
 	// Register the handlers for the directories and specify the HTTP method. '/{$}' is used so that home is no longer a catch-all - a 404 will be returned instead
 	mux.HandleFunc("GET /{$}", home)
 	mux.HandleFunc("GET /snippet/create", snippetCreate)
