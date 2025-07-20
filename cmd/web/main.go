b4f1a4682f40c93037c8f32687c508f51852b910
zakkbob <114868973+zakkbob@users.noreply.github.com>
2025-07-20 20:56:05 +0100

    Chapter 2.10 - The http.Handler interface
    Demonstrate how mux.HandleFunc is syntactic sugar

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -18,9 +18,11 @@ func main() {
 
 	// Register the handlers for the directories and specify the HTTP method. '/{$}' is used so that home is no longer a catch-all - a 404 will be returned instead
 	mux.HandleFunc("GET /{$}", home)
-	mux.HandleFunc("GET /snippet/create", snippetCreate)
 	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
 	mux.HandleFunc("GET /snippet/view/{id}", snippetView) // {id} is a wildcard route pattern, it'll match any non-empty value in that segment. Also, use 'id' instead of 'snippetID' as this avoids 'stutter'
+
+	// mux.HandleFunc is syntactic sugar for this, so we can just use this directly instead
+	mux.Handle("GET /snippet/create", http.HandlerFunc(snippetCreate))
 
 	log.Print("starting server on :4000")
 
