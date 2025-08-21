e52b68e56e458ca96a76c69db4d7525ec42af7c2
zakkbob <114868973+zakkbob@users.noreply.github.com>
2025-08-21 14:13:33 +0100

    Chapter 4.4 - Creating a database pool
    yep, i created a database pool

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,26 +1,39 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log/slog"
 	"net/http"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
-// Holds application wide dependencies, allowing for dependency injection
+// Holds application wide dependencies, allowing for
+// dependency injection
 type application struct {
 	logger *slog.Logger
 }
 
 func main() {
-	// Get network address from flag
-	addr := flag.String("addr", ":4000", "HTTP Network Address")
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	dsn := flag.String("dsn", "web:pass@/snippetbox?parseTime=true", "MySQL data source name")
 
-	// Parse the command-line flags. Otherwise the default value will remain. This is why flag.String() returns a pointer
+	// Parse the command-line flags. Otherwise the default value will remain.
+	// This is why flag.String() returns a pointer
 	flag.Parse()
 
 	// Initialise a new structured logger, along with its handler
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+	db, err := openDB(*dsn)
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+
+	defer db.Close()
 
 	app := &application{
 		logger: logger,
@@ -28,9 +41,26 @@ func main() {
 
 	logger.Info("starting server", "address", *addr)
 
-	// Start a new web server on configured port, using the servemux we just created
-	// Then, log an error if we get one, slog doesn't have an equivalent to Fatal(), so calling os.Exit(1) is necess:ary
-	err := http.ListenAndServe(*addr, app.routes())
+	// Log an error if we get one, slog doesn't have an equivalent to Fatal(),
+	// so calling os.Exit(1) is necessary
+	err = http.ListenAndServe(*addr, app.routes())
 	logger.Error(err.Error())
 	os.Exit(1)
+}
+
+// Wraps sql.Open() and returns a sql.DB connection pool
+// for a given DSN.
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		db.Close()
+		return nil, err
+	}
+
+	return db, nil
 }
