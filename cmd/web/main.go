27a39c999dd815b0bd8b3b790480383e0780dba6
zakkbob <114868973+zakkbob@users.noreply.github.com>
2025-08-25 12:35:48 +0100

    Chapter 8.2 - Setting up the session manager
    Install and integrate session manager

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,9 +7,12 @@ import (
 	"log/slog"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/zakkbob/snippetbox/internal/models"
 
+	"github.com/alexedwards/scs/mysqlstore"
+	"github.com/alexedwards/scs/v2"
 	"github.com/go-playground/form/v4"
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -17,10 +20,11 @@ import (
 // Holds application wide dependencies, allowing for
 // dependency injection
 type application struct {
-	logger        *slog.Logger
-	snippets      *models.SnippetModel
-	templateCache map[string]*template.Template
-	formDecoder   *form.Decoder
+	logger         *slog.Logger
+	snippets       *models.SnippetModel
+	templateCache  map[string]*template.Template
+	formDecoder    *form.Decoder
+	sessionManager *scs.SessionManager
 }
 
 func main() {
@@ -47,11 +51,16 @@ func main() {
 
 	formDecoder := form.NewDecoder()
 
+	sessionManager := scs.New()
+	sessionManager.Store = mysqlstore.New(db)
+	sessionManager.Lifetime = 12 * time.Hour
+
 	app := &application{
-		logger:        logger,
-		snippets:      &models.SnippetModel{DB: db},
-		templateCache: templateCache,
-		formDecoder:   formDecoder,
+		logger:         logger,
+		snippets:       &models.SnippetModel{DB: db},
+		templateCache:  templateCache,
+		formDecoder:    formDecoder,
+		sessionManager: sessionManager,
 	}
 
 	logger.Info("starting server", "address", *addr)
--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -7,23 +7,20 @@ import (
 )
 
 func (app *application) routes() http.Handler {
-	// Initialise a 'servemux', this is where route handlers will be registered
 	mux := http.NewServeMux()
 
-	// Create a file server, to serve files out of ui/static
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 
 	// Register fileServer as the handler for all paths starting with '/static/'
 	// Remove the 'static/' prefix before it reaches the fileserver
 	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
 
-	// Register the handlers for the directories and specify the HTTP method. '/{$}' is used so that home is no longer a catch-all - a 404 will be returned instead
-	mux.HandleFunc("GET /{$}", app.home)
-	mux.HandleFunc("POST /snippet/create", app.snippetCreatePost)
-	mux.HandleFunc("GET /snippet/view/{id}", app.snippetView) // {id} is a wildcard route pattern, it'll match any non-empty value in that segment. Also, use 'id' instead of 'snippetID' as this avoids 'stutter'
+	dynamic := alice.New(app.sessionManager.LoadAndSave)
 
-	// mux.HandleFunc is syntactic sugar for this, so we can just use this directly instead
-	mux.Handle("GET /snippet/create", http.HandlerFunc(app.snippetCreate))
+	mux.Handle("GET /{$}", dynamic.ThenFunc(app.home))
+	mux.Handle("POST /snippet/create", dynamic.ThenFunc(app.snippetCreatePost))
+	mux.Handle("GET /snippet/view/{id}", dynamic.ThenFunc(app.snippetView))
+	mux.Handle("GET /snippet/create", dynamic.ThenFunc(app.snippetCreate))
 
 	standard := alice.New(app.recoverPanic, app.logRequest, commonHeaders)
 
