84b0bd9ef3612c1ec710d01b30f31c5c322a9d40
zakkbob <114868973+zakkbob@users.noreply.github.com>
2025-08-25 13:09:13 +0100

    Chapter 8.3 - Working with session data
    Add flash message upon snippet creation

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -32,6 +32,7 @@ func (app *application) decodePostForm(r *http.Request, dst any) error {
 func (app *application) newTemplateData(r *http.Request) templateData {
 	return templateData{
 		CurrentYear: time.Now().Year(),
+		Flash:       app.sessionManager.PopString(r.Context(), "flash"),
 	}
 }
 
