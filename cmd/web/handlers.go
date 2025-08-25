429fe8079baaa31353a5d23f7417a3555b8df883
zakkbob <114868973+zakkbob@users.noreply.github.com>
2025-08-25 15:30:39 +0100

    Chapter 10.3 - User signup and password encryption
    Implement user signup process

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -49,14 +49,52 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 	app.render(w, r, http.StatusOK, "view.tmpl.html", data)
 }
 
+type userSignupForm struct {
+	Name                string `form:"name"`
+	Email               string `form:"email"`
+	Password            string `form:"password"`
+	validator.Validator `form:"-"`
+}
+
 // GET '/user/signup' - Create an account (form)
 func (app *application) userSignup(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Create an account (form)")
+	data := app.newTemplateData(r)
+	data.Form = userSignupForm{}
+	app.render(w, r, http.StatusOK, "signup.tmpl.html", data)
 }
 
 // POST '/user/signup' - Create an account
 func (app *application) userSignupPost(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Create an account")
+	var form userSignupForm
+	app.decodePostForm(r, &form)
+
+	form.CheckField(validator.NotBlank(form.Name), "name", "This field cannot be blank")
+	form.CheckField(validator.NotBlank(form.Email), "email", "This field cannot be blank")
+	form.CheckField(validator.Matches(form.Email, validator.EmailRX), "email", "This field must be a valid email address")
+	form.CheckField(validator.NotBlank(form.Password), "password", "This field cannot be blank")
+	form.CheckField(validator.MinChars(form.Password, 8), "password", "This field must be at least 8 characters long")
+
+	if !form.Valid() {
+		data := app.newTemplateData(r)
+		data.Form = form
+
+		app.render(w, r, http.StatusUnprocessableEntity, "signup.tmpl.html", data)
+	}
+
+	err := app.users.Insert(form.Name, form.Email, form.Password)
+	if err != nil {
+		if errors.Is(err, models.ErrDuplicateEmail) {
+			form.AddFieldError("email", "Email address is already in use")
+
+			data := app.newTemplateData(r)
+			data.Form = form
+			app.render(w, r, http.StatusUnprocessableEntity, "signup.tmpl.html", data)
+		}
+		app.serverError(w, r, err)
+	}
+
+	app.sessionManager.Put(r.Context(), "flash", "Your signup was successful. Please log in.")
+	http.Redirect(w, r, "/user/login", http.StatusSeeOther)
 }
 
 // GET '/user/login' - Login to an account (form)
@@ -108,7 +146,7 @@ func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request
 	if !form.Valid() {
 		data := app.newTemplateData(r)
 		data.Form = form
-		app.render(w, r, http.StatusOK, "create.tmpl.html", data)
+		app.render(w, r, http.StatusUnprocessableEntity, "create.tmpl.html", data)
 		return
 	}
 
--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/go-playground/form/v4 v4.2.1
 	github.com/go-sql-driver/mysql v1.9.3
 	github.com/justinas/alice v1.2.0
+	golang.org/x/crypto v0.41.0
 )
 
 require filippo.io/edwards25519 v1.1.0 // indirect
